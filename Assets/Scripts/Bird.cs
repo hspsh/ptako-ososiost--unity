e602f68577c6b1de4cf1879b5229d12236b862ae
WysockiPatryk <wysockipawelpatryk@gmail.com>
2016-08-12 19:30:36 +0200

    initial commit

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour {
+
+    protected Rigidbody rigidbodyComponent;
+
+    public float PtakoPrędkość;
+    public float PtakoZwrotność;
+
+	// Use this for initialization
+	void Start () {
+        rigidbodyComponent = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void FlyForward(float flyingInput) {
+        Vector3 direction = (transform.rotation * Vector3.forward) * PtakoPrędkość * flyingInput;
+        Debug.Log("asd " + direction);
+        rigidbodyComponent.AddForce(direction);
+    }
+
+    public void RotateRoll(float rotation) {
+        rigidbodyComponent.AddTorque(new Vector3(rotation * PtakoZwrotność, 0, 0));
+    }
+
+    public void RotatePitch(float rotation) {
+        rigidbodyComponent.AddTorque(new Vector3(0, rotation * PtakoZwrotność, 0));
+    }
+
+    public void RotateYaw(float rotation) {
+        rigidbodyComponent.AddTorque(new Vector3(0, 0, rotation * PtakoZwrotność));
+    }
+}
---
+++ Assets/Scripts/KeyboardControls.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardControls : MonoBehaviour {
+
+    public Bird birdToControl;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessMovement();
+    }
+    void ProcessMovement() {
+        birdToControl.FlyForward(Input.GetAxis("Vertical"));
+        birdToControl.RotatePitch(Input.GetAxis("Horizontal"));
+    }
+}
