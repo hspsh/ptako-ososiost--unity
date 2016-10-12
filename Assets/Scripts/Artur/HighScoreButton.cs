ad8b899c24a740c161bd0299764c16d23c78dcb5
Mises543 <artursobol50@gmail.com>
2016-10-12 10:27:26 +0200

    BirdController1
    Marge project and did first bird controller.

---
+++ Assets/Scripts/Artur/HighScoreButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class HighScoreButton : MonoBehaviour {
+
+	public void onClick ()
+    {
+        //Application.LoadLevel(2);
+        SceneManager.LoadScene(2);
+    }
+
+}
